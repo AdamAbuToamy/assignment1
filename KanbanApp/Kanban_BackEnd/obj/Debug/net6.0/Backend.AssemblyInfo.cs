57857420ea2a817b969e83f835059945a81defa3
zeenyaseen <zeenashraf2003@gmail.com>
2024-06-12 21:39:12 +0300

    a

--- KanbanApp/Kanban_BackEnd/obj/Debug/net6.0/Backend.AssemblyInfo.cs
+++ KanbanApp/Kanban_BackEnd/obj/Debug/net6.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26f65544a1211ea7483d75d44d362ca53e9237d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40e6be8da26c6bb1fd53b00c89648208a83be3e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
